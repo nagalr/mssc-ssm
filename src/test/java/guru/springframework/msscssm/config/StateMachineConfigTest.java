30127a47a369b761120196daf90ea85f8e48b8b9
Ronnen Nagal <nagalr@gmail.com>
2021-07-29 18:50:08 +0300

    Complete a test class, StateMachineConfigTest with clear log-output

--- src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
+++ src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
@@ -22,18 +22,19 @@ class StateMachineConfigTest {
 
     @Test
     void testNewStateMachine() {
+
         StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID().toString());
         sm.start();
 
-        System.out.println("Current State: " + sm.getState().toString());
+        System.out.println("Initial State: " + sm.getState().getId().toString());
 
         sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
 
-        System.out.println("After First State Change: " + sm.getState().toString());
+        System.out.println("After First State Change: " + sm.getState().getId().toString());
 
         sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
 
-        System.out.println("After Second State Change: " + sm.getState().toString());
+        System.out.println("After Second State Change: " + sm.getState().getId().toString());
    }
 
 

ed622b31540626047a2c82586572b30e11d4ff75
Ronnen Nagal <nagalr@gmail.com>
2021-07-29 17:21:08 +0300

    Completes the StateMachineConfig class, overwrite the 'configure' method.

--- src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
 
 /**
  * Created by ronnen on 29-Jul-2021
@@ -16,5 +19,13 @@ import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 @EnableStateMachineFactory
 public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
 
-
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.AUTH_ERROR)
+                .end(PaymentState.PRE_AUTH_ERROR);
+    }
 }

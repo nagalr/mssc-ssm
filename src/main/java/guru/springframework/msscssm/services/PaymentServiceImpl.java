c88e6d632a2a0778456d6a2740889a736a2753bf
Ronnen Nagal <nagalr@gmail.com>
2021-07-30 14:51:38 +0300

    Add Payment Service Interface, and it's Impl

---
+++ src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
@@ -0,0 +1,44 @@
+package guru.springframework.msscssm.services;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import guru.springframework.msscssm.repository.PaymentRepository;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ronnen on 30-Jul-2021
+ */
+
+@Service
+@RequiredArgsConstructor
+@Builder
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+
+    @Override
+    public Payment newPayment(Payment payment) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        return null;
+    }
+
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        return null;
+    }
+}

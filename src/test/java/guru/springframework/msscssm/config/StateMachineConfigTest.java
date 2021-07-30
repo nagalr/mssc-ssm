496b839f2e641c02c1f26e5d93200cfe04e7900f
Ronnen Nagal <nagalr@gmail.com>
2021-07-30 11:32:52 +0300

    Add comments to the test class.

--- src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
+++ src/test/java/guru/springframework/msscssm/config/StateMachineConfigTest.java
@@ -25,17 +25,19 @@ class StateMachineConfigTest {
     @Test
     void testNewStateMachine() {
 
+        // 'getStateMachine' - Build a new StateMachine instance with a given machine uuid, returns it.
         StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID().toString());
         sm.start();
 
+        // the initial state is 'NEW'
         System.out.println("Initial State: " + sm.getState().getId().toString());
 
+        // send an event to trigger a state change. ('PRE_AUTHORIZE' event does not change state)
         sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
-
         System.out.println("After First State Change: " + sm.getState().getId().toString());
 
+        // send an event to trigger a state change.
         sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
-
         System.out.println("After Second State Change: " + sm.getState().getId().toString());
 
         assertEquals("PRE_AUTH", sm.getState().getId().toString());
